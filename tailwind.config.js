5eeaf2a553627c215066e70554756c1942be785b
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-04 23:13:52 +0000

    update tailwind to JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-	purge: ["./src/**/*.njk"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+    mode: "jit",
+    purge: ["./src/**/*.njk"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "brand-blue": "blue",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
