85572b1524213170ccb65858e05308671b0aa42e
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-05 22:07:14 +0000

    random emoji

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
-console.log("%cDon't read my console ;]", "color:salmon;font-size:40px");
+const emojis = ["❤️‍🔥", "🧜‍♀️", "🛼", "👩🏼‍❤️‍💋‍👨🏻", "🌺"];
+const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+console.log(
+  `%cHere's a random emoji my guy. ${randomEmoji}`,
+  "color:salmon;font-size:40px"
+);
