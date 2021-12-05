6e6ce140481f0f974f8d0fd141fd55a89fd50001
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-05 21:48:59 +0000

    esbuild

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -1,1 +1,4 @@
-console.log("%cDon't read my console ;]", "color:salmon;font-size:40px");
+(() => {
+  // src/scripts/main.js
+  console.log("%cDon't read my console ;]", "color:salmon;font-size:40px");
+})();
