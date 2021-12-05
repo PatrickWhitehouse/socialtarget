3ca221b1822cfb0bc4b6b2aafbaf92c8eb8e1c83
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-05 22:01:42 +0000

    remove dates now it's working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,1 @@
-import { formatDistance, subDays } from "date-fns";
-
-console.log(
-  formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
-);
 console.log("%cDon't read my console ;]", "color:salmon;font-size:40px");
