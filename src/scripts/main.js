05914f964c461ffbc1d573b8b7773da595d2f8b6
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-05 21:56:23 +0000

    test esbuild

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
+import { formatDistance, subDays } from "date-fns";
+
+console.log(
+  formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+);
 console.log("%cDon't read my console ;]", "color:salmon;font-size:40px");
