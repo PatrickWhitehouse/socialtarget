dc1926263cc4e13448f5f9e3b2e2a736c134bd62
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-05 21:13:22 +0000

    add js rollup

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/styles/tailwind.css");
-    eleventyConfig.addPassthroughCopy("./src/images/");
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-        },
-    };
+  eleventyConfig.addWatchTarget("./src/styles/tailwind.css");
+  eleventyConfig.addWatchTarget("./src/scripts/main.js");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/scripts/bundle.js");
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
