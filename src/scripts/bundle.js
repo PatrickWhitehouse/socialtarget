1834fc6f305134499c0ff3a69aab86df5354acea
Patrick Whitehouse <patrick1996@gmail.com>
2021-12-11 21:56:35 +0000

    new content

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -1,19 +1,7 @@
 (() => {
   // src/scripts/main.js
-  var emojis = [
-    "\u2764\uFE0F\u200D\u{1F525}",
-    "\u{1F9DC}\u200D\u2640\uFE0F",
-    "\u{1F6FC}",
-    "\u{1F469}\u{1F3FC}\u200D\u2764\uFE0F\u200D\u{1F48B}\u200D\u{1F468}\u{1F3FB}",
-    "\u{1F33A}",
-  ];
+  var emojis = ["\u2764\uFE0F\u200D\u{1F525}", "\u{1F9DC}\u200D\u2640\uFE0F", "\u{1F6FC}", "\u{1F469}\u{1F3FC}\u200D\u2764\uFE0F\u200D\u{1F48B}\u200D\u{1F468}\u{1F3FB}", "\u{1F33A}"];
   var randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-  console.log(
-    `%cHere's a random emoji my guy. ${randomEmoji}`,
-    "color:salmon;font-size:40px"
-  );
-  console.log(
-    `%cSite built by @patrickdoesweb`,
-    "color:lavendar;font-size:40px"
-  );
+  console.log(`%cHere's a random emoji my guy. ${randomEmoji}`, "color:salmon;font-size:40px");
+  console.log(`%cSite built by @patrickdoesweb`, "color:lavendar;font-size:40px");
 })();
